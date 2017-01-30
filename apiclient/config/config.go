054cee9cfd35533f8abaf1cab788bd4247ed1c07
James Neve <jamesoneve@gmail.com>
2017-01-30 11:48:05 +0900

    Fix formatting

--- apiclient/config/config.go
+++ apiclient/config/config.go
@@ -1,13 +1,13 @@
 package config
 
 const (
-       endpointApp = "https://app.datarobot.com"
+	endpointApp = "https://app.datarobot.com"
 )
 
 // Config has auth data for datarobot api
 type Config struct {
 	HostName string
-        Key      string
+	Key      string
 	User     string
 	Token    string
 	Password string
@@ -15,11 +15,11 @@ type Config struct {
 
 // NewWithToken returns initialized Config with api token
 func NewWithToken(user, token, hostName, key string) Config {
-	       h := hostName
-	       k := key
-	       if h == "" {
-		       h = endpointApp
-	       }
+	h := hostName
+	k := key
+	if h == "" {
+		h = endpointApp
+	}
 
 	return Config{
 		HostName: h,
